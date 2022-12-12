ca9ba3b7934b3702fb7eee685cf9437ef4d82cbc
Kotty <kotty@guns-n-girls.de>
2022-12-12 09:55:29 +0100

    some little fixes

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DAEMON_NAME=${SCRIPT_DIR##*/}
 
-kill $(pgrep -f "python $SCRIPT_DIR/dbus-shelly-3em-smartmeter.py")
+kill $(pgrep -f "python $SCRIPT_DIR/$DAEMON_NAME")
