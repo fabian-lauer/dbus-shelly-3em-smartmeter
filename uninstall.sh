fe37f34084b4e4ce6c324b33de59a74e0b521358
Fabian Lauer <101807694+fabian-lauer@users.noreply.github.com>
2023-02-27 21:30:15 +0100

    Update on scripts
    Had a little problem with the "#!/bin/bash" - had to remove the LF+CR

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  
 DAEMON_NAME=${SCRIPT_DIR##*/}
 
 rm /service/$DAEMON_NAME
+kill $(pgrep -f "python $SCRIPT_DIR/$DAEMON_NAME")
 kill $(pgrep -f 'supervise $DAEMON_NAME')
 chmod a-x $SCRIPT_DIR/service/run
 $SCRIPT_DIR/restart.sh
